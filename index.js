03a6b91deec591eab010f6c6b09690bd914edb24
Agung Maulana <iamaul@hotmail.com>
2020-04-21 16:35:02 +0700

    sets access-control-allow-headers

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(express.json({ extended: false }));
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-auth-token");
     next();
 });
 
