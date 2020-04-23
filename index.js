346edf93b6e650946a98abd93a817a219590585b
Agung Maulana <iamaul@hotmail.com>
2020-04-23 22:10:50 +0700

    enabled cors access

--- index.js
+++ index.js
@@ -25,6 +25,12 @@ const app = express();
 
 app.use(express.json({ extended: false }));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://101.50.3.61:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
 app.get('/', (req, res) => res.send({ status: 'success', message: 'EvoniX Backend API v1.' }));
 app.use('/api/v1/auth', auth);
 app.use('/api/v1/users', user);
