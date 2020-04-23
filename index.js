58d71f43a58dd8f9dcc7b4ea7a0c86564152decb
Agung Maulana <iamaul@hotmail.com>
2020-04-24 05:28:59 +0700

    removed cors headers

--- index.js
+++ index.js
@@ -23,12 +23,6 @@ const character = require('./routes/api/character');
 
 const app = express();
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); 
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-});
-
 app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send({ status: 'success', message: 'EvoniX Backend API v1.' }));
