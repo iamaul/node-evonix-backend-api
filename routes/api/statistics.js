91e33d83e2b4d27f8c1bbe972b54eda5395903cb
Agung Maulana <iamaul@hotmail.com>
2020-04-25 00:31:37 +0700

    added validationResult express

--- routes/api/statistics.js
+++ routes/api/statistics.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const { Op, DataTypes } = require('sequelize');
+const { validationResult } = require('express-validator');
 
 // Connection
 const database = require('../../config/database');
