8b7283384938beffafb6452a87b6ca85b54a6f55
Agung Maulana <iamaul@hotmail.com>
2020-04-24 13:10:41 +0700

    updated cors request

--- index.js
+++ index.js
@@ -23,6 +23,12 @@ const character = require('./routes/api/character');
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://101.50.3.61:3000');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send({ status: 'success', message: 'EvoniX Backend API v1.' }));
