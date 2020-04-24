398b5592d5ee978750881e6254c1d11dc6ebf9b2
Agung Maulana <iamaul@hotmail.com>
2020-04-24 19:51:26 +0700

    fixed a router middleware on statistics.js

--- routes/api/statistics.js
+++ routes/api/statistics.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+
 const { Op, DataTypes } = require('sequelize');
 
 // Connection
@@ -97,3 +98,5 @@ router.get('/stats/player_properties', async (req, res) => {
         });
     }
 });
+
+module.exports = router;
