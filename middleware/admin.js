3d14a653690d46bc1458ef604fc285bb32787b3f
Agung Maulana <iamaul@hotmail.com>
2020-05-05 02:59:07 +0700

    edited middleware status 401 msg

--- middleware/admin.js
+++ middleware/admin.js
@@ -2,6 +2,6 @@ module.exports = function(req, res, next) {
     if (req.user.admin >= 1) {
         next();
     } else {
-        return res.status(401).json({ status: false, msg: 'You\'re not authorized to access this page.' });
+        return res.status(401).json({ status: false, msg: '401 Not Authorized.' });
     }    
 }
