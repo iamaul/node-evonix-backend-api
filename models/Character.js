48fe0897c1de90d420db712aa1197e151e6fb11d
Agung Maulana <iamaul@hotmail.com>
2020-05-22 18:07:04 +0700

    updated latest changes

--- models/Character.js
+++ models/Character.js
@@ -17,7 +17,7 @@ const Character = database.define('Character', {
     name: { type: DataTypes.STRING(24) },
     lastlogin: { type: DataTypes.INTEGER },
     skin_id: { type: DataTypes.INTEGER },
-    gender: { type: DataTypes.STRING(8) },
+    gender: { type: DataTypes.TINYINT },
     birth_day: { type: DataTypes.MEDIUMINT },
     birth_month: { type: DataTypes.MEDIUMINT },
     birth_year: { type: DataTypes.INTEGER },
