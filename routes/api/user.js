f5f555f169854edeffa272fcd1000245fe329771
Agung Maulana <iamaul@hotmail.com>
2020-05-31 21:45:12 +0700

    updated latest changes

--- routes/api/user.js
+++ routes/api/user.js
@@ -166,25 +166,7 @@ router.post('/email/verification', auth, async (req, res) => {
                                                         <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;">
                                                             <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;">Hi <b>${user.name}</b>,</p>
                                                             <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;">To verify your email, please click the following link below:</p>
-                                                            <table border="0" cellpadding="0" cellspacing="0" class="btn btn-primary" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; box-sizing: border-box;">
-                                                                <tbody>
-                                                                    <tr>
-                                                                        <td align="left" style="font-family: sans-serif; font-size: 14px; vertical-align: top; padding-bottom: 15px;">
-                                                                            <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: auto;">
-                                                                                <tbody>
-                                                                                    <tr>
-                                                                                        <td style="font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: #ff0000; border-radius: 5px; text-align: center;"> 
-                                                                                            <a href="http://101.50.3.61:3000/email/verification/${user_session.code}" target="_blank" style="display: inline-block; color: #ffffff; background-color: #ff0000; border: solid 1px #ff0000; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #ff0000;">
-                                                                                                Confirm Email Verification
-                                                                                            </a>
-                                                                                        </td>
-                                                                                    </tr>
-                                                                                </tbody>
-                                                                            </table>
-                                                                        </td>
-                                                                    </tr>
-                                                                </tbody>
-                                                            </table>
+                                                            <p>http://101.50.3.61:3000/email/verification/${user_session.code}</p>
                                                         </td>
                                                     </tr>
                                                 </table>
