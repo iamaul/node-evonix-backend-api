7c91b5c4fd09fb09d61492dad54d1afec2a791d6
Agung Maulana <iamaul@hotmail.com>
2020-06-03 22:36:48 +0700

    updated latest changes

--- models/Faction.js
+++ models/Faction.js
@@ -10,7 +10,13 @@ const Faction = database.define('Faction', {
     name: { type: DataTypes.STRING(60) },
     alias: { type: DataTypes.STRING(30) },
     type: { type: DataTypes.TINYINT },
-    leader_sqlid: { type: DataTypes.INTEGER },
+    leader_sqlid: {
+        type: DataTypes.INTEGER(11),
+        references: {
+            model: Character,
+            key: 'id'
+        }
+    },
     rank_member: { type: DataTypes.TINYINT },
     rank_manager: { type: DataTypes.TINYINT },
     rank_leader: { type: DataTypes.TINYINT },
