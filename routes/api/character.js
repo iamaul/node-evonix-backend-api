35476baf25e891779d50863719c1f91cb53c8de1
Agung Maulana <iamaul@hotmail.com>
2020-06-08 18:22:17 +0700

    updated latest changes

--- routes/api/character.js
+++ routes/api/character.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+const moment = require('moment');
 const { check, validationResult } = require('express-validator');
 
 // Middleware
