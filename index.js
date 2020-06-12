da165c7342848df56f45a4a6f7074473dcb68758
Agung Maulana <iamaul@hotmail.com>
2020-06-12 17:23:10 +0700

    updated CORS methods

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ const news = require('./routes/api/news');
 const app = express();
 
 var corsMiddleware = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://167.99.65.76:3000');
-    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, PATCH, POST, DELETE');
+    res.header('Access-Control-Allow-Origin', 'http://167.99.65.76');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');
     next();
 }
