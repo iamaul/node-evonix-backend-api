2660aaf71acb44ddd025de38f271c33cd9c2d88d
Agung Maulana <iamaul@hotmail.com>
2021-03-20 02:30:52 +0700

    updated latest changes

--- routes/api/samp.js
+++ routes/api/samp.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const axios = require('axios');
+var sampquery = require('samp-query');
+var options = {
+    host: '13.212.169.37'
+}
 
 /**
  * @route   GET /api/v1/server
@@ -12,6 +16,21 @@ router.get('/', async (req, res) => {
     try {
         const { data: info } = await axios.get('https://api.open.mp/server/13.212.169.37:7777'); 
         return res.status(201).json(info);
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).json({
+            errors: [{
+                status: false,
+                msg: error.message
+            }]
+        });
+    }
+});
+
+router.get('/new', async (req, res) => {
+    try {
+        const data = await sampquery(options);
+        return res.status(201).json(data);
     } catch (error) {
         console.error(error.message);
         return res.status(500).json({
