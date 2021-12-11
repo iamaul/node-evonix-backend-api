37ef34c44fb3f1eb24122e90382a80767e7baff1
Agung Maulana <iamaul@hotmail.com>
2021-12-12 01:47:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.use('/api/v1/quiz', quiz);
 app.use('/api/v1/ban', ban);
 app.use('/api/v1/news', news);
 
-const SERVER_PORT = process.env.PORT || 5000;
+const SERVER_PORT = 8080;
 
 app.listen(SERVER_PORT, () =>
     console.log(`[log] evonix-backend-api started running in ${process.env.NODE_ENV} mode on port ${SERVER_PORT}`)
